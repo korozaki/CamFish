ddf7840b7bf2d571529c3bd8aec74f1795deaf98
korozaki <68153795+korozaki@users.noreply.github.com>
2020-09-01 13:12:20 +0200

    Update camphish.sh

--- camphish.sh
+++ camphish.sh
@@ -15,7 +15,7 @@ printf "\e[1;92m | |      | (   ) || |   | |\e[0m\e[1;77m| (      | (   ) |   | 
 printf "\e[1;92m | (____/\| )   ( || )   ( |\e[0m\e[1;77m| )      | )   ( |___) (___/\____) || )   ( |\e[0m\n"
 printf "\e[1;92m (_______/|/     \||/     \|\e[0m\e[1;77m|/       |/     \|\_______/\_______)|/     \|\e[0m\n\n"
 
-printf " \e[1;77m https://www.youtube.com/channel/UCPdeJOzfA_NE-ITMI5WnZsg \e[0m \n"
+printf " \e[1;77m Mr Kali | https://www.youtube.com/channel/UCPdeJOzfA_NE-ITMI5WnZsg \e[0m \n"
 
 printf "\n"
 
